e30e0ffc8cd50b1e317e7c1c2fe7aab93b2a4611
murashov.s <angrycat2017@gmail.com>
2026-04-07 10:15:29 +0300

    homework done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c
+  if (d < 0) {
+	  return arr
+  } else if (d === 0) {
+	  let r = -b / (2 * a)
+	  arr.push(r)
+  } else if (d > 0) {
+	  let r1 = (-b + Math.sqrt(d) ) / (2 * a)
+	  let r2 = (-b - Math.sqrt(d) ) / (2 * a)
+	  arr.push(r1, r2)
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentageMonthly = percent / 12 / 100
+  let creditBody = amount - contribution
+  let paymentMontly = creditBody * (percentageMonthly + (percentageMonthly / (((1 + percentageMonthly) ** countMonths) - 1)))
+  let paymentTotal = paymentMontly * countMonths
+  return parseFloat(paymentTotal.toFixed(2))
 }
