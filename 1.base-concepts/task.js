6bb7850a63aec0f069bbd74ba6e5739828c15a2f
murashov.s <angrycat2017@gmail.com>
2026-04-09 11:02:50 +0300

    Добавил точки с запятой

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,31 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b ** 2 - 4 * a * c
+  let d = b ** 2 - 4 * a * c;
+
   if (d < 0) {
-	  return arr
+    return arr;
   } else if (d === 0) {
-	  let r = -b / (2 * a)
-	  arr.push(r)
+    let r = -b / (2 * a);
+    arr.push(r);
   } else if (d > 0) {
-	  let r1 = (-b + Math.sqrt(d) ) / (2 * a)
-	  let r2 = (-b - Math.sqrt(d) ) / (2 * a)
-	  arr.push(r1, r2)
+    let r1 = (-b + Math.sqrt(d)) / (2 * a);
+    let r2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(r1, r2);
   }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentageMonthly = percent / 12 / 100
-  let creditBody = amount - contribution
-  let paymentMontly = creditBody * (percentageMonthly + (percentageMonthly / (((1 + percentageMonthly) ** countMonths) - 1)))
-  let paymentTotal = paymentMontly * countMonths
-  return parseFloat(paymentTotal.toFixed(2))
+  let percentageMonthly = percent / 12 / 100;
+  let creditBody = amount - contribution;
+  let paymentMontly = creditBody * (
+    percentageMonthly +
+    (percentageMonthly / (((1 + percentageMonthly) ** countMonths) - 1))
+  );
+  let paymentTotal = paymentMontly * countMonths;
+
+  return parseFloat(paymentTotal.toFixed(2));
 }
