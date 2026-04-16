e0da6dedb86fc181d8d995e300e6e41cc2ec86e5
murashov.s <angrycat2017@gmail.com>
2026-04-16 14:08:19 +0300

    Сделал ДЗ

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length != arr2.length) {
+    return false;
+  } else {
+	return arr1.every((element, index) => {
+	  return element === arr2[index];
+    })
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const ages = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age);
+
+  if (ages.length === 0) return 0;
+
+  const sum = ages.reduce((acc, age) => acc + age, 0);
+
+  return sum / ages.length;
 }
