a4e5ee3e129f5331c04bd103317644dadfd54a99
murashov.s <angrycat2017@gmail.com>
2026-04-18 12:43:33 +0300

    Сделал ДЗ

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
+
+let student1 = new Student("Alex", "male", 23);
+let student2 = new Student("Bob", "male", 26);
+let student3 = new Student("Jessica", "female", 20);
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+    if (this.marks) {
+        this.marks.push(...marks);
+    } else {
+        return 0;
+    }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+
+    let marksSum = this.marks.reduce((a, b) => a + b, 0);
+    return marksSum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
