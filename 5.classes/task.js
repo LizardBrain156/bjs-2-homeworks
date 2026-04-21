22203148a7ce58fbf01e690044f60cb118df7aa4
murashov.s <angrycat2017@gmail.com>
2026-04-21 12:41:47 +0300

    Выполнил ДЗ

---
+++ 5.classes/task.js
@@ -0,0 +1,105 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+
+    set state(value) {
+        if (value < 0) {
+            this._state = 0;
+        } else if (value > 100) {
+            this._state = 100;
+        } else {
+            this._state = value;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "book";
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            if (book[type] === value) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                return this.books.splice(i, 1)[0];
+            }
+        }
+        return null;
+    }
+}
+
+let library = new Library("Russian State Library");
+library.addBook(new NovelBook("George Martin", "A Game Of Thrones", 1996, 694));
+library.addBook(new FantasticBook("James Corey", "Leviathan Wakes", 2011, 577));
+library.addBook(new NovelBook("Andrzej Sapkowski", "The Last Wish", 1990, 288));
+library.addBook(new Book("John Reed", "Ten Days that Shook the World", 1919, 400));
+console.log(library.findBookBy("releaseDate", 1919));
+let theBook = library.giveBookByName("Ten Days that Shook the World");
+theBook.state = 99;
+theBook.fix();
+library.addBook(theBook);
